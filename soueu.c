78642f1d84af19b26d3fe3fd81d57a6da4ae8965
Saria <sandy.vitoriabarros@gmail.com>
2019-11-07 17:53:32 -0400

    commit

---
+++ soueu.c
@@ -0,0 +1,32 @@
+//Programa: soueu.c
+//Programadora: Sandy
+//Trabalho de criptografia
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+
+int main(int argc, char *argv[]){
+
+    char *op;
+    op = argv[1];
+    char a=op[0];
+
+    switch(a){
+        case 'F'://Fabio
+        break;
+
+        case 'P': //Patricia
+        break;
+
+        case 'T': //Teodoro
+        break;
+
+        case 'E': //Ester
+        break;
+    }
+
+
+    return 0;
+}
