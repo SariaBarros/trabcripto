0c55c1cfd7d9892dab5cbb398d50cda1dc69906c
Saria <sandy.vitoriabarros@gmail.com>
2019-11-08 21:07:34 -0400

    versão simples

--- euclides.c
+++ euclides.c
@@ -18,11 +18,32 @@ int MDC(int a, int b){
     }
 }
 
+int EuclidesE(int a, int b){
+    int oR, nR, oA, nA, oB, nB; //oR="old resto"; nR="new resto"; oA:"antigo coeficiente de A"
+    int auxA, auxB, auxR;
+    oR = a; nR = b; oA = 1; nA = 0; oB = 0; nB = 1; 
+    while(nR != 0){
+        auxA = nA; 
+        nA = oA - ((oR/nR)*nA);
+        auxB = nB;
+        nB = oB - ((oR/nR)*nB);
+        auxR = nR;
+        nR = oR - ((oR/nR)*nR);
+        
+        oA = auxA;
+        oB = auxB;
+        oR = auxR;
+    }
+
+        printf("a:%d e b: %d\n", nA, nB);
+    return 1;
+}
+
 int main(void){
     int a, b, r;
     scanf("%d %d", &a, &b);
 
-    r = MDC(a, b);
+    r = EuclidesE(a, b);
 
     printf("%d\n", r);
 
